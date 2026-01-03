fix: update confidence normalizing

src/model_edge_v5/dataset.py
@@ -1,5 +1,11 @@
 import random
-# FLIP_INDICES = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15, 18, 17, 20, 19, 22, 21, 24, 23, 26, 25, 28, 27, 30, 29, 32, 31]
+import os
+import json
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+import torchvision.transforms.functional as TF
 
 # FLIP FOR COCO17
 FLIP_INDICES = [